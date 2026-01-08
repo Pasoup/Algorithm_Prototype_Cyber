0adbe10ccd8594b4eefd77f915a1f86e134e97e8
K-n15 <theepapon7055@gmail.com>
2026-01-08 18:37:45 +0700

    Installed FastAPI and create api path for checking submitted url

--- Link_algo.py
+++ Link_algo.py
@@ -1,7 +1,20 @@
-import re, ssl, socket, whois, datetime, Levenshtein
+import ssl, socket, whois, datetime, requests
 from urllib.parse import urlparse
 from datetime import datetime,UTC,timezone
-import requests
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class LinkRequest(BaseModel):
+    url:str
+
+@app.post("/checkURL")
+def checkURL(request:LinkRequest):
+    checker = ScamDetector()
+    RiskLevel,RiskFactor = checker.analyze_url_algo(request.url)
+    return {"RiskLevel":RiskLevel,"RiskFactor":RiskFactor}
+
 class  ScamDetector:
     def __init__(self):
         self.target_brands = ["google", "facebook", "amazon", "paypal", "apple", "netflix", "scb"]
@@ -36,23 +49,23 @@ class  ScamDetector:
         except ssl.SSLCertVerificationError as e:
             #This is bad, SSLCert expired which could mean phishing risk, data exposure, etc.
             print(f"SSL ERROR: The certificate is invalid! ({e.verify_message})")
-            return 100, "SSL certificate expired"
+            return 30, "SSL certificate expired"
         except socket.gaierror:
             #Could not find server address, could be phishing attempt
             print(f"DNS ERROR: Could not find server {root}")
-            return 100, "Server does not exist"
+            return 30, "Server does not exist"
         except socket.timeout:
             #The server is either slow or blocked, doesn't necessary mean it's dangerous
             #Perhaps it's some company private server which weren't meant for public
             print("Timeout Error (Server slow/blocked)")
-            return 50, "Server could be slow or blocked"
+            return 15, "Server could be slow or blocked"
         except ConnectionRefusedError:
             print("Connection refused (Port close)")
-            return 80, "Connection refused"
+            return 24, "Connection refused"
         except Exception as e:
             #Unknown error be assess as risky
             print(f"ERROR: {e}")
-            return 100, "Unknown Error"
+            return 30, "Unknown Error"
 
     def get_domain_from_url(self, url):
         #extract keywords from url
@@ -91,8 +104,6 @@ class  ScamDetector:
             return age.days
         except Exception as e:
             return f"Error: {str(e)}"
-        
-
 
         #---checking for redirect
     def Redirect_analyze(self, url):
