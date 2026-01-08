e48d8c6bf3af75062d0896c0c9ee6b941f13bd43
K-n15 <theepapon7055@gmail.com>
2026-01-08 13:56:27 +0700

    Created SSLcertification check

--- Link_algo.py
+++ Link_algo.py
@@ -1,19 +1,44 @@
-import re
-import whois
-import datetime
-import Levenshtein
+import re, ssl, socket, whois, datetime, Levenshtein
 from urllib.parse import urlparse
+from datetime import datetime,UTC,timezone
 
 class ScamDetector:
     def __init__(self):
         self.target_brands = ["google", "facebook", "amazon", "paypal", "apple", "netflix", "scb"]
-        
         self.suspicious_keywords = ["login", "verify", "secure", "account", "update", "banking", "signin", "confirm"]
 
-    def analyze_url(sefl, link):
+    def analyze_url(self, link):
         pass
 
-    
+    def SSL_analyze(self, url):
+        parse = urlparse(url)
+        root = parse.hostname
+        context = ssl.create_default_context()
+        try:
+            with socket.create_connection((root, 443), timeout=3.0) as sock:
+                with context.wrap_socket(sock, server_hostname=root) as ssock:
+                    #Handshake done
+                    cert = ssock.getpeercert()
+                    #Extracting detail
+                    subject = list(cert['subject'])
+                    issuer = list(cert['issuer'])
+                    expire = cert['notAfter']
+                    expire_date = datetime.strptime(expire, r"%b %d %H:%M:%S %Y %Z").replace(tzinfo=timezone.utc)
+                    days_left = (expire_date - datetime.now(UTC)).days
+                    print(f"Host name : {root}")
+                    print(f"Issued by : {issuer}")
+                    print(f"Expire date : {expire_date}")
+                    print(f"Day till expire : {days_left}")
+        except ssl.SSLCertVerificationError as e:
+            #This is bad, SSLCert expired
+            print(f"SSL ERROR: The certificate is invalid! ({e.verify_message})")
+        except socket.gaierror:
+            #Could not find server address
+            print(f"DNS ERROR: Could not find server {root}")
+        except Exception as e:
+            #Could be a misconfiguration
+            print(f"ERROR: {e}")
+
 if __name__ == "__main__":
     detector = ScamDetector()
 
@@ -26,6 +51,4 @@ if __name__ == "__main__":
     ]
 
     for link in test_urls:
-        percent, log = detector.analyze_url(link)
-        print(f" Link: {link}")
-        print(f" Risk Score: {percent}%")
+        detector.SSL_analyze(link)
